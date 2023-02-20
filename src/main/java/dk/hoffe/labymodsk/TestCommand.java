996e35c5d9214fc117f216236dbc3b4df6c774b8
Itz_Hoffe <mhoff770@gmail.com>
2023-02-20 22:21:52 +0100

    Example af media lige

--- src/main/java/dk/hoffe/labymodsk/TestCommand.java
+++ src/main/java/dk/hoffe/labymodsk/TestCommand.java
@@ -1,10 +1,13 @@
 package dk.hoffe.labymodsk;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import net.labymod.opus.OpusCodec;
 import org.apache.commons.codec.binary.Base64;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -23,12 +26,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class TestCommand implements CommandExecutor, PluginMessageListener {
-    private static AudioFormat format =
-            new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 48000, 16, 1, 2, 48000, false);
+    public TestCommand() {
+        Main.getInstance().getServer().getMessenger().registerIncomingPluginChannel(Main.getInstance(), "labymod3:media", this);
+    }
     private List<String> files = Arrays.asList("test.ogg", "test2.ogg");
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        List<String> files = Arrays.asList("test.ogg", "test2.ogg");
         Player player = (Player) commandSender;
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("type", "play");
@@ -52,7 +55,7 @@ public class TestCommand implements CommandExecutor, PluginMessageListener {
 
     @Override
     public void onPluginMessageReceived(String channel, Player player, byte[] message) {
-        if (!channel.equals("labymod3:sound")) {
+        if (!channel.equals("labymod3:media")) {
             return;
         }
 
@@ -61,7 +64,18 @@ public class TestCommand implements CommandExecutor, PluginMessageListener {
         String json = MediaProtocol.readString(buf, Short.MAX_VALUE);
 
         if(key.equals("done")) {
+            JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
+            if(jsonObject.get("id").getAsString().equals(files.get(0))) {
+                JsonObject jsonObject2 = new JsonObject();
+                jsonObject2.addProperty("type", "play");
+                byte[] bytes = getBytesFromFile(files.get(1));
+                String compressBase64 = Base64.encodeBase64String(bytes);
+                jsonObject2.addProperty("data", compressBase64);
+                jsonObject2.addProperty("id", files.get(1));
+                //player.sendMessage(jsonObject.toString());
+                MediaProtocol.sendLabyModMessage(player, "sound", jsonObject2);
 
+            }
         }
     }
 }
